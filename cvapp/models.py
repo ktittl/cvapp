64b3f35f8d18e205a57d73889ef91dcdf26f0b87
ktittl <kinga.tittl@gmail.com>
2020-04-29 10:20:01 +0200

    Add required models and apply code improvements

--- cvapp/models.py
+++ cvapp/models.py
@@ -1,16 +1,45 @@
-from cvapp import db
+from cvapp.app import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    cv_url = db.Column(db.String())
+    # skill # todo
 
     def __repr__(self):
-        return f"User({self.id}, '{self.username}')"
+        return f"User({self.id}, '{self.first_name}', '{self.last_name}', '{self.cv_url}')"
 
     @property
     def serialize(self):
         return {
-            'id': self.id,
-            'username': self.username
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "cv_url": self.cv_url
         }
+
+    @staticmethod
+    def required_properties():
+        return "first_name", "last_name", "cv_url"
+
+
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"Skill({self.id}, '{self.name}')"
+
+
+class UserSkillAssociations(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", foreign_keys=[user_id])
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'), nullable=False)
+    skill = db.relationship("Skill", foreign_keys=[skill_id])
+    level = db.Column(db.Integer)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint(user_id, skill_id),
+    )
--- cvapp/routes.py
+++ cvapp/routes.py
@@ -1,12 +1,7 @@
-from cvapp import app
-from cvapp import db
+from cvapp.app import app, db
 from cvapp.models import User
 from flask import jsonify, request
-
-
-@app.route("/")
-def home():
-    return "<h1>CV database app</h1>"
+from sqlalchemy.exc import IntegrityError
 
 
 @app.route("/api/user", methods=['GET'])
@@ -16,14 +11,19 @@ def get_all_users():
 
 @app.route("/api/user", methods=['POST'])
 def create_user():
-    if not request.json or not "username" in request.json:
-        return "Invalid body", 400
-    user = User(username=request.json["username"])
-    db.session.add(user)
-    db.session.commit()
-    id = user.id
-    user = User.query.get(id)
-    return jsonify(user.serialize), 201
+    if not request.json:
+        return "Missing body", 400
+    if not all(key in request.json for key in User.required_properties()):
+        return f"Body does not contain all required properties: {User.required_properties()}", 400
+
+    try:
+        user = User(**request.json)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(user.serialize), 201
+    except IntegrityError:
+        db.session.rollback()
+        return "This user already exist", 400
 
 
 @app.route("/api/user/<int:user_id>", methods=['GET'])
@@ -34,7 +34,12 @@ def get_user(user_id):
 
 @app.route("/api/user/<int:user_id>", methods=['DELETE'])
 def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return "User deleted"
+    try:
+        user = User.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return f"User with id={user_id} was not found", 404
+
+    return jsonify(user.serialize)
