4e1a511889930c78fecaf4eea1d82570ba1d3f87
ktittl <kinga.tittl@gmail.com>
2020-04-30 14:22:23 +0200

    Add skills for user

--- cvapp/models.py
+++ cvapp/models.py
@@ -6,10 +6,11 @@ class User(db.Model):
     first_name = db.Column(db.String())
     last_name = db.Column(db.String())
     cv_url = db.Column(db.String())
-    # skill # todo
+    skill_associations = db.relationship("UserSkillAssociations", backref="user", lazy=True,
+                                         cascade="all, delete-orphan")
 
     def __repr__(self):
-        return f"User({self.id}, '{self.first_name}', '{self.last_name}', '{self.cv_url}')"
+        return f"User({self.id}, '{self.first_name}', '{self.last_name}', '{self.cv_url}', '{self.skill_associations}')"
 
     @property
     def serialize(self):
@@ -17,29 +18,46 @@ class User(db.Model):
             "id": self.id,
             "first_name": self.first_name,
             "last_name": self.last_name,
-            "cv_url": self.cv_url
+            "cv_url": self.cv_url,
+            "skills": [s.serialize for s in self.skill_associations]
         }
 
     @staticmethod
-    def required_properties():
-        return "first_name", "last_name", "cv_url"
+    def required_keys():
+        return "first_name", "last_name", "cv_url", "skills"
 
 
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), unique=True, nullable=False)
 
     def __repr__(self):
         return f"Skill({self.id}, '{self.name}')"
 
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
 
 class UserSkillAssociations(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship("User", foreign_keys=[user_id])
-    skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, primary_key=True)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'), nullable=False, primary_key=True)
     skill = db.relationship("Skill", foreign_keys=[skill_id])
     level = db.Column(db.Integer)
 
-    __table_args__ = (
-        db.PrimaryKeyConstraint(user_id, skill_id),
-    )
+    def __repr__(self):
+        return f"Skill({self.skill.name}, '{self.level}')"
+
+    @property
+    def serialize(self):
+        return {
+            "name": self.skill.name,
+            "level": self.level
+        }
+
+    @staticmethod
+    def required_keys():
+        return "name", "level"
--- cvapp/routes.py
+++ cvapp/routes.py
@@ -1,7 +1,8 @@
 from cvapp.app import app, db
-from cvapp.models import User
+from cvapp.models import User, Skill, UserSkillAssociations
 from flask import jsonify, request
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import DetachedInstanceError
 
 
 @app.route("/api/user", methods=['GET'])
@@ -13,11 +14,22 @@ def get_all_users():
 def create_user():
     if not request.json:
         return "Missing body", 400
-    if not all(key in request.json for key in User.required_properties()):
-        return f"Body does not contain all required properties: {User.required_properties()}", 400
+    if not all(key in request.json for key in User.required_keys()):
+        return f"Body must contain required properties: {User.required_keys()}", 400
 
     try:
+        skills = request.json.pop("skills")
         user = User(**request.json)
+        skill_associations = []
+        for s in skills:
+            if not all(key in s for key in UserSkillAssociations.required_keys()):
+                return f"Property 'skill' must contain required keys: {UserSkillAssociations.required_keys()}", 400
+            skill = Skill.query.filter_by(name=s["name"]).first()
+            if not skill:
+                return f"Given skill '{s['name']}' does not exist", 400
+            skill_associations.append(UserSkillAssociations(user_id=user.id, skill=skill, level=s["level"]))
+        user.skill_associations = skill_associations
+
         db.session.add(user)
         db.session.commit()
         return jsonify(user.serialize), 201
@@ -35,11 +47,33 @@ def get_user(user_id):
 @app.route("/api/user/<int:user_id>", methods=['DELETE'])
 def delete_user(user_id):
     try:
-        user = User.get_or_404(user_id)
+        user = User.query.get_or_404(user_id)
+        user.skill_associations = []
         db.session.delete(user)
         db.session.commit()
-    except IntegrityError:
+        return jsonify(user.serialize)
+    except IntegrityError or DetachedInstanceError:
         db.session.rollback()
         return f"User with id={user_id} was not found", 404
 
-    return jsonify(user.serialize)
+
+@app.route("/api/skill", methods=['POST'])
+def create_skill():
+    if not request.json:
+        return "Missing body", 400
+    if not "name" in request.json:
+        return f"Body does not contain required property: 'name'", 400
+
+    try:
+        skill = Skill(**request.json)
+        db.session.add(skill)
+        db.session.commit()
+        return jsonify(skill.serialize), 201
+    except IntegrityError:
+        db.session.rollback()
+        return "This skill already exist", 400
+
+
+@app.route("/api/skill", methods=['GET'])
+def get_all_skills():
+    return jsonify([skill.serialize for skill in Skill.query.all()])
